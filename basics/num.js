d1edb711c0f8ba27272c00f33952f51c2ac714ff
Rajdeep Dey <146769108+gitcohub@users.noreply.github.com>
2025-07-13 07:58:07 +0000

    num

---
+++ basics/num.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const othernum =1123.45691
+// console.log(othernum.toPrecision(5));
+
+
+// const hundred = 2000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++MATHS++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.max(4,6,8,2));
+// console.log(Math.min(2,6,1,7,9));
+
+console.log(Math.random());
+console.log(Math.random()*100 + 1);
+console.log(Math.floor(Math.random()*100) + 1);
