6937ce8308df04f8fe88917fec882bb254ef63b5
Rajdeep Dey <146769108+gitcohub@users.noreply.github.com>
2025-07-13 07:59:14 +0000

    data_types2

---
+++ basics/data_types2.js
@@ -0,0 +1,20 @@
+// let myYt = "deyraj.com"
+
+// let anothername = myYt
+// anothername = "chaiaurcode"
+
+// console.log(myYt);
+// console.log(anothername);
+
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email = "raj@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
